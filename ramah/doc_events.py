2ebad5034567db159f4231e6cbe31b738c9b7933
nilesh-greycube <nilesh@greycube.in>
2026-02-26 17:46:22 +0530

    fix: added logic for case if height exceeds max height in settings

--- ramah/doc_events.py
+++ ramah/doc_events.py
@@ -8,15 +8,19 @@ def get_settings_data(height, hole_type, qty):
     return_hole_qty = 0
 
     if hole_type == "One Hole":
-        for i in table:
-            if i.get("to_height") >= height:
-                return_hole_qty = i.get("one_hole_qty")
+        for row in table:
+            if row.get("to_height") >= height:
+                return_hole_qty = row.get("one_hole_qty")
                 break
+            else:
+                return_hole_qty = row.get("one_hole_qty")
     if hole_type == "Two Hole":
-        for i in table:
-            if i.get("to_height") >= height:
-                return_hole_qty = i.get("two_hole_qty")
+        for row in table:
+            if row.get("to_height") >= height:
+                return_hole_qty = row.get("two_hole_qty")
                 break
+            else:
+                return_hole_qty = row.get("two_hole_qty")
 
     return {
         "hole_qty" : return_hole_qty,
@@ -27,6 +31,6 @@ def get_settings_data(height, hole_type, qty):
 def validate_child_items(doc, method):    
     items = doc.items
 
-    for i in items:
-        if i.rate < i.custom_less_rate_for_sale:
-            frappe.throw(f"<p>You have entered wrong value of <b>Rate</b> for item in row <b>{i.idx}</b>.</p><p>It should be more than <b>{i.custom_less_rate_for_sale}</b></p>")
+    for row in items:
+        if row.rate < row.custom_less_rate_for_sale:
+            frappe.throw(f"<p>You have entered wrong value of <b>Rate</b> for item in row <b>{row.idx}</b>.</p><p>It should be more than <b>{row.custom_less_rate_for_sale}</b></p>")
